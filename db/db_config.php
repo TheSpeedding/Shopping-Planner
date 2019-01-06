2023f4fc092fd37afe7f5ed25b23870dab3e03d7
Lukáš Riedel <thespeedding@gmail.com>
2019-01-06 10:09:45 +0100

    Update db_config.php

--- db/db_config.php
+++ db/db_config.php
@@ -1,7 +1,7 @@
 <?php
   $db_config = array(
-    'server'   => 'localhost',
-    'login'    => 'riedell',
-    'password' => 'xDROSLcs',
-    'database' => 'stud_riedell'
+    'server'   => '',
+    'login'    => '',
+    'password' => '',
+    'database' => ''
    );
